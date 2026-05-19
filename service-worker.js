fcbae484917c91b20a713d5b93975d5e4caa1350
seumedeiros <meu406h@gmail.com>
2026-05-18 22:08:25 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,36 @@
-const CACHE_NAME = "diario-gamer-v1";
+
+const CACHE_NAME = "pwa-cache-v1";
 
 const urlsToCache = [
-  "./",
-  "./index.html"
+"./",
+"./index.html",
+"./manifest.json",
+"./icon-192.png",
+"./icon-512.png"
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install",event=>{
 
-  event.waitUntil(
+event.waitUntil(
 
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+caches.open(CACHE_NAME)
+.then(cache=>cache.addAll(urlsToCache))
 
-  );
+);
 
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch",event=>{
 
-  event.respondWith(
+event.respondWith(
 
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+caches.match(event.request)
+.then(response=>{
 
-  );
+return response || fetch(event.request);
+
+})
+
+);
 
 });
